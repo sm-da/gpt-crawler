4d5fa3b64eb6cc34969f4aa14f5aacd48c50a51c
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-07 12:25:19 +0000

    Update file

--- config.ts
+++ config.ts
@@ -192,7 +192,7 @@ export const defaultConfig: Config = {
   ],
   url: "https://www.whitehouse.gov/",
   match: "https://www.whitehouse.gov/**",
-  maxPagesToCrawl: 999999999999,
+  maxPagesToCrawl: 99999999999,
   outputFileName: "output/white-house.json",
   maxTokens: 2000000,
   selector: "body",

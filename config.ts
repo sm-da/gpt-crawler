5661fdb2bfef06c39927b75758428f6cecafee66
supermario-ai <supermario@supermarioai.onmicrosoft.com>
2024-12-06 19:01:24 -0600

    fix: update default configuration for URL and output file path

--- config.ts
+++ config.ts
@@ -1,9 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://",
+  match: "https://**",
+  maxPagesToCrawl: 1,
+  outputFileName: "output/<filename>.json",
   maxTokens: 2000000,
+  selector: "body"
 };

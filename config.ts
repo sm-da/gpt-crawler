caeb7756709a6b002bb0ffd4bf64d08dc1ff9645
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-08 11:42:28 +0000

    Update file

--- config.ts
+++ config.ts
@@ -192,7 +192,7 @@ export const defaultConfig: Config = {
   ],
   url: "https://www.riotgames.com/",
   match: "https://www.riotgames.com/**",
-  maxPagesToCrawl: 9999999999999,
+  maxPagesToCrawl: 999999999999,
   outputFileName: "output/riot-games.json",
   maxTokens: 2000000,
   selector: "body",

119713a8adead0b6e5dc286477db8f4f25b8e8e5
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 04:38:03 +0000

    Update file

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://paypal.com",
+  url: "https://paypal.com/",
   match: "https://paypal.com/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "paypal.json",

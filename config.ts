4edd6be258d703787099b73b176ff632a3f668f4
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 05:56:41 +0000

    Update file

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://neuralink.com/",
   match: "https://neuralink.com/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "neuralink.json",
   maxTokens: 2000000,
 };

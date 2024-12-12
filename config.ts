c14912575ec9ffa0b4de40dc0e22aaec7c881d73
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 06:03:59 +0000

    Update file

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://docs.openai.com/",
-  match: "https://docs.openai.com/**",
+  url: "https://platform.openai.com/docs/overview",
+  match: "https://platform.openai.com/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "open-ai.json",
   maxTokens: 2000000,

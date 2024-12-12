71b64d0666f35c2b6cefc1556f0d378b73a47f03
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 04:19:01 +0000

    Update file

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://spacecoinageminecryps.com/",
   match: "https://spacecoinageminecryps.com/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "output.json",
   maxTokens: 2000000,
   selector: "body",

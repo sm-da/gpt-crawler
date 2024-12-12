c58a4fbc152dd87237e5cbb31c21754133fa1bbb
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 04:04:13 +0000

    Update file

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  url: "https://www.spacex.com/",
+  match: "https://www.spacex.com/**",
+  maxPagesToCrawl: 9999999999999,
   outputFileName: "output.json",
   maxTokens: 2000000,
 };
